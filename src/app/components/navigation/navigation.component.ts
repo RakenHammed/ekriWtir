824d1d9a6d626dce8b8a259c3e964c2865af4c14
Raken <hammed.raken123@gmail.com>
2019-05-07 23:56:19 +0100

    add user can log out option in the menu

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,14 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-navigation',
-    templateUrl: './navigation.component.html',
-    styleUrls: ['./navigation.component.scss']
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
+  localStorage: string;
+  constructor() {
 
-    constructor() { }
+  }
 
-    ngOnInit() {}
+  ngOnInit() { }
+
+  isloggedIn(): boolean {
+    if (localStorage.getItem('user')) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  logOut() {
+    localStorage.clear();
+  }
 
 }
